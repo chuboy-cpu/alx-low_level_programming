20b6aa666c337ef0f24bbe243939b0f75d48a9b2
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-08 18:32:35 +0100

    create

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("%d",sizeof(int));
+    printf("%d",sizeof(char));
+    printf("%d",sizeof(float));
+    printf("%d",sizeof(long int));
+    printf("%d",sizeof(double));
+    return 0;
+}
