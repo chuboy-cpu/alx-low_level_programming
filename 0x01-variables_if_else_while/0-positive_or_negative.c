edb49231366aa855fe1db312c1f20b06211e0300
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-09 21:56:39 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /**
  * main - Entry piont
- * 
+ * Return: 0
  */
 
 /* betty style doc for function main goes there */
@@ -17,17 +17,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 
-if (n < 0)
-{
+    if (n < 0)
+    {
     printf("%d is negative\n", n);
-}
-else if (n > 0)
-{
+    }
+    else if (n > 0)
+    {
     printf("%d is positive\n", n);
-}
-else
-{
+    }
+    else
+    {
     printf("%d is zero\n", n);
-}
+    }
 return (0);
 }
