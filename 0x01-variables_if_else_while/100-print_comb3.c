3972535145208f03c91d935aa1ffd11b30ab358f
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-09 16:24:49 +0100

    wow

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; ++i)
+	{
+		for (j = j + 1; j <= 57; j++)
+		{
+			putchar((char)i);
+			putchar((char)j;
+			if (i != 56)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 55; ++i)
+	{
+		for (j = i + 1; j <= 56; j++)
+		{
+            for (k = j +1; k <= 57; k++)
+            {
+                putchar((char)i);
+			    putchar((char)j);
+                putchar((char)k);
+			    if (i != 55)
+			    {   
+			    	putchar(',');
+			    	putchar(' ');
+			    }
+            }
+			
+		}
+	}
+		putchar('\n');
+		return (0);
+}
