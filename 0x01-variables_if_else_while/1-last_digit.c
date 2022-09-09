b8f2ca6a082096ddc2692d1691a37c9474e63010
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-09 22:30:23 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,9 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-int last_digit = n % 10;
+int last_digit;
+
+last_digit = n % 10;
 printf("Last digit of %d is l %d ", n, last_digit);
 if (n > 5)
 {
