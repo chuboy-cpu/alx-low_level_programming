5161eb4602efaf0797f58b30e45798d661e88d40
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-13 21:35:58 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,7 +7,7 @@
  * Return: void
  */
 int fibonacci(int, int, int);
-int i = 0;
+
 int main(void)
 {
 fibonacci(0, 1, 50);
@@ -16,7 +16,8 @@ return (0);
 
 int fibonacci(int f,  int b, int n)
 {
-int sum;
+int sum, i;
+i = 0;
 while (i < n) /*n is how long we want the fibonacci series will go*/
 {
 sum = f + b;
@@ -24,6 +25,6 @@ printf("%d, ", sum);
 f = b;
 b = sum;
 ++i;
-};
+}
 return (0);
 }
