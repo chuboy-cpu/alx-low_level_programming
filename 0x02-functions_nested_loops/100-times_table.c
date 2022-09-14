9481361325fd2c38f0a75497dd737abab5940e56
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-14 05:24:36 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,21 +9,21 @@
 void print_times_table(int n)
 {
 int i, j;
+if (n > 15 || n < 0)
+{
+_putchar(' ');
+}
+else
+{
 for (i = 0; i <= n; ++i)
 {
 for (j = 0; j <= n; ++i)
-{
-if (n > 15 || n < 0)
-{
-_putchar('0');
-}
-else
 {
 _putchar((i * j));
 _putchar(',');
 _putchar(' ');
 }
-_putchar('\n');
 }
+_putchar('\n');
 }
 }
