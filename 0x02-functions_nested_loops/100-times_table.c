39962bd92376589452189f1bc1524fc6ea7ac39f
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-14 19:57:27 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,54 +8,48 @@
 
 void print_times_table(int n)
 {
-int i, j;
 if (n > 15 || n < 0)
-{
-}
 else
 {
-for (i = 0; i <= n; ++i)
+for (int i = 0; i <= n; ++i)
 {
-for (j = 0; j <= n; ++j)
+for (int j = 0; j <= n; ++j)
 {
-int mul; 
-mul = i * j;
+int mul;mul = i * j;
 if (mul <= 9)
 {
 _putchar('0' + mul);
+if (j != n)
+{
+_putchar(',');
+}
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
 }
 else if (mul < 100)
 {
 _putchar('0' + (mul / 10));
 _putchar('0' + (mul % 10));
+if (j != n)
+{
+_putchar(',');
+}
+_putchar(' ');
+_putchar(' ');
 }
 else
 {
 _putchar('0' + (mul / 100));
 _putchar('0' + (mul % 100) / 10);
 _putchar('0' + (mul % 100) % 10);
-}
 if (j != n)
 {
 _putchar(',');
-if ((mul + i) < 10)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
 }
-else if ((mul + i) < 100)
-{
 _putchar(' ');
-_putchar(' ');
-}
-else
-{
-_putchar(' ');
-}
 }
 }
 _putchar('\n');
-}
 }
 }
