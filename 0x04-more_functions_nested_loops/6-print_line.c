d992709c766580d02fde359a67743527de32604f
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-15 15:30:21 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,6 +2,7 @@
 
 /**
  * print_line - print numbers from 0-9 ecept 2 and 4
+ * @n: chara
  * Return: void
  */
 
@@ -10,13 +11,12 @@ void print_line(int n)
 int i;
 if (n <= 0)
 {
-_putchar('\n')
+_putchar('\n');
 }
 else
 {
 for (i = 0; i < n; ++i)
 {
-
 _putchar('_');
 }
 _putchar('\n');
