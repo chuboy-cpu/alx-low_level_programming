54d4ff03683e7163ed9f27d57013d67d67738dea
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-15 17:03:05 +0100

    create

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,16 @@ printf("%d", 1);
 for (i = 2; i <= 100; ++i)
 {
 printf(" ");
-if ((i % 3 == 0) && (i % 5 == 0))
+if (i % 3 == 0 || i % 5 == 0)
 {
-printf("FizzBuzz");
+if (i % 3 == 0)
+{
+printf("Fizz");
 }
-else if (i % 5 == 0)
+if (i % 5 == 0)
 {
 printf("Buzz");
 }
-else if (i % 3 == 0)
-{
-printf("Fizz");
 }
 else
 {
