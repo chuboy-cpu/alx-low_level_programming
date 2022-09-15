5b50f0fc1f23fdede9fe4b985612f1123cbc1813
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-15 12:59:09 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -10,20 +10,5 @@
 int mul(int a, int b)
 {
 mult = a * b;
-if (mult <= 9)
-{
-_putchar(mult);
-}
-else if (mult < 100)
-{
-_putchar('0' + mult / 10);
-_putchar('0' + mult % 10);
-}
-else
-{
-_putchar('0' + mult / 100);
-_putchar('0' + (mult % 100) / 10);
-_putchar('0' + (mult % 100) % 10);
-}
-return (0);
+return (mult);
 }
