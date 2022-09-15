f55fa2f2f958c064e709475bc034322fc6bd1128
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-15 11:01:08 +0100

    fix

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isdigit - islower
+ *@c: character to be checked
+ *Return: 0
+ */
+
+int _isdigit(int c)
+{
+if ((c / 1 == c)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
