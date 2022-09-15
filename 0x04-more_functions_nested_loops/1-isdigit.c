cae1ff3c2092bb65b04c150ad7e3b5a563b565eb
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-15 12:31:30 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isdigit - islower
  * @c: character to be checked
  * Return: 0
  */
-int main(void)
-{
-_isdigit('A');
-}
-
 int _isdigit(int c)
 {
-if ((c / 1 == c)
+if ((c / 1) == c)
 {
 return (1);
 }
