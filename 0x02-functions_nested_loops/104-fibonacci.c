465ed19897da7f0c08d0a12c886bcd7d8118d727
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-15 03:24:39 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,6 +4,13 @@
  * main - Entry piont
  * Return: void
  */
+int fibonacci(unsigned long, unsigned long, unsigned long);
+
+int main(void)
+{
+fibonacci(0, 1, 95);
+return (0);
+}
 
 int fibonacci(unsigned long f,  unsigned long b, unsigned long n)
 {
