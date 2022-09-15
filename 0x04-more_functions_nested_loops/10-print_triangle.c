2a9024ce2a5c3748f6c151181eb2ba79a2977f7d
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-15 17:12:22 +0100

    create

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: chara
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+c = size;
+for (j = 0; j < size; ++j)
+{
+i = 0;
+while (i < c)
+{
+_putchar(' ');
+++i;
+}
+_putchar('#');
+--c;
+_putchar('\n');
+}
+_putchar('\n')
+}
+}
