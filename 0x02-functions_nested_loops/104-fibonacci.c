4165fdbcf2ff8f067cd606f423eee8abc17265b9
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-15 03:33:08 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,25 +2,35 @@
 
 /**
  * main - Entry piont
+ * @long: chara
+ * @long: chara
+ * @long: chara
  * Return: void
  */
 int fibonacci(unsigned long, unsigned long, unsigned long);
 
 int main(void)
 {
-fibonacci(0, 1, 95);
+fibonacci(0, 1, 97);
 return (0);
 }
+
+/**
+ * fibonacci - returns fibanocci series
+ * @f: chra
+ * @b: chara
+ * @n: chara
+ * Return: int
+ */
 
 int fibonacci(unsigned long f,  unsigned long b, unsigned long n)
 {
 unsigned long sum, i;
-
+i = 0;
 while (i < n) /*n is how long we want the fibonacci series will go*/
 {
 sum = f + b;
-i = 0;
-printf("%lu, ",sum);
+printf("%lu, ", sum);
 f = b;
 b = sum;
 ++i;
