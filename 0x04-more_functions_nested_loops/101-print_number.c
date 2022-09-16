1553f9a10c20de45a809ecac9a8bd92e290e14c8
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-16 09:51:49 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,12 +10,25 @@ void print_number(int n)
 {
 if (n <= 9)
 { 
-_putchar('0' + mul);
+_putchar('0' + n);
 }
 else if (n < 100)
 {
 _putchar('0' + (n / 10));
 _putchar('0' + (n % 10));
+}
+else if (n < 1000)
+{
+_putchar('0' + (n / 100));
+_putchar('0' + (n % 100) / 10);
+_putchar('0' + (n % 100) % 10);
+}
+else
+{
+_putchar('0' + (n / 1000));
+_putchar('0' + (n % 1000) / 100);
+_putchar('0' + ((n % 1000) % 100) / 10);
+_putchar('0' + ((n % 1000) % 100) % 10);
 }
 _putchar('\n');
 }
