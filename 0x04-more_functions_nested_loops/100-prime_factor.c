b56c4d71f9cd6726250e540e059f59b403a77d41
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-16 08:34:36 +0100

    fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 
 /**
  * main - Entry piont
@@ -9,20 +8,20 @@
 
 int main(void)
 {
-long double check_prime;
-long double i, num, max;
-num = 612852475143;
+unsigned long int check_prime;
+unsigned long int i, num, max;
+num = 6128;
 max = 0;
 for (i = 2; i < num; ++i)
 {
 int prime;
-if (floor(remainder(num, i)) == 0)
+if (num % i == 0)
 {
-if (remainder(i, 2) != 0 || i == 2)
+if (i % 2 != 0 || i == 2)
 {
 for (check_prime = 3; check_prime < num; check_prime += 2)
 {
-if (remainder(i, check_prime) == 0 && i != check_prime)
+if (i % check_prime == 0 && i != check_prime)
 {
 prime = 1;
 continue;
@@ -38,6 +37,6 @@ max = i;
 }
 }
 }
-printf("The max prime factor %0.lf \n", max);
+printf("The max prime factor %ld \n", max);
 return (0);
 }
