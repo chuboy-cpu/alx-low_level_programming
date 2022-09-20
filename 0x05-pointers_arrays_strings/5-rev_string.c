860ea47d87cb6f818f1b5a62353d6ede85bdd5af
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-20 15:28:47 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,37 +7,16 @@
 
 void rev_string(char *n)
 {
-int i, j, k;
-char *hold = " ";
+int i, j;
+char hold;
 j = (_strlen(n) - 1);
 /*saving th first half of the string in hold*/
 for (i = 0; i <= (j / 2); ++i)
 {
-*(hold + i) = *(n + i);
+hold = *(n + i);
+*(n + i) = *(n + j - i);
+*(n + j - i) = hold;
 }
-/*putting the second half into the first half*/
-k = 0;
-for (i = j; i >= (j / 2); --i)
-{
-while (k < i)
-{
-*(n + k) = *(n + i);
-++k;
-break;
-}
-}
-/*putting the saved half from hold into the second*/
-k = 0;
-for (i = j; i >= (j / 2); --i)
-{
-while (k < i)
-{
-*(n + i) = *(hold + k);
-++k;
-break;
-}
-}
-_putchar('\n');
 }
 
 /**
@@ -56,3 +35,27 @@ for (i = 0; *(n + i) != '\0'; ++i)
 }
 return (j);
 }
+
+/*
+**k = 0;
+for (i = j; i >= (j / 2); --i)
+{
+while (k < i)
+{
+*(n + k) = *(n + i);
+++k;
+break;
+}
+}
+/*putting the saved half from hold into the second*/
+/*k = 0;
+for (i = j; i >= (j / 2); --i)
+{
+while (k < i)
+{
+*(n + i) = *(hold + k);
+++k;
+break;
+}
+}
+/*_putchar('\n');*/
