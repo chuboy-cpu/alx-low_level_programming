e835a71677026bfdba7cdc5edd708662ed925c8d
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-20 16:07:36 +0100

    Update 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * puts_half - function
+ * @n: pionter
+ */
+
+void puts_half(char *j)
+{
+int i, len;
+len = _strlen(j);
+if (len % 2 == 0)
+{
+n = len;
+}
+else
+{
+n = len - 1
+}
+for (i = n; i > (n / 2); --i)
+{
+_putchar(*(j + i));
+}
+_putchar('\n');
+}
+
+/**
+ * _strlen - function
+ * @n: pionter
+ * Return: j
+ */
+
+int _strlen(char *n)
+{
+int i, j;
+j = 0;
+for (i = 0; *(n + i) != '\0'; ++i)
+{
+++j;
+}
+return (j);
+}
