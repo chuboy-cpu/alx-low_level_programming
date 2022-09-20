b729b53ff4d741c687ca50f28f3445307968aa2a
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-20 10:45:48 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,14 @@
 /**
  * _strlen - function
  * @n: pionter
+ * Return: j
  */
 
 int _strlen(char *n)
 {
 int i, j;
-j =0;
-for (i =0; *(n + i) != '\0'; ++i)
+j = 0;
+for (i = 0; *(n + i) != '\0'; ++i)
 {
 ++j;
 }
