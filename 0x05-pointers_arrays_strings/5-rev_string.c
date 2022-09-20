ea2b68c60f0aaf731e4e3b96d03f1de64c9205b9
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-20 15:37:48 +0100

    fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -35,29 +35,3 @@ for (i = 0; *(n + i) != '\0'; ++i)
 }
 return (j);
 }
-
-/**
-*@description: experiment
-*k = 0;
-*for (i = j; i >= (j / 2); --i)
-*{
-*while (k < i)
-*{
-*(n + k) = *(n + i);
-*++k;
-*break;
-*}
-*}
-*putting the saved half from hold into the second
-*k = 0;
-*for (i = j; i >= (j / 2); --i)
-*{
-*while (k < i)
-*{
-*(n + i) = *(hold + k);
-*++k;
-*break;
-*}
-*}
-*_putchar('\n');
-*/
