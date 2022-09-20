c9503351d48bd76dc0a3699f6620bf97897a5ace
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-20 15:42:26 +0100

    create

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - function
+ * @n: pionter
+ */
+
+void puts2(char *n)
+{
+int i;
+for (i = 0; *(n + i) != '\0'; ++i)
+{
+_putchar(*(n + i));
+}
+_putchar('\n');
+}
