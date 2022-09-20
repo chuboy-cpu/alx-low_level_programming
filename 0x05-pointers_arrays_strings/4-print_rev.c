e3175dbfda635671ac767bc06ff3e165d07c3bc1
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-20 10:54:06 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,3 +15,20 @@ _putchar(*(n + i));
 }
 _putchar('\n');
 }
+
+/**
+ * _strlen - function
+ * @n: pionter
+ * Return: j
+ */
+
+int _strlen(char *n)
+{
+int i, j;
+j = 0;
+for (i = 0; *(n + i) != '\0'; ++i)
+{
+++j;
+}
+return (j);
+}
