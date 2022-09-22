7f862ae098359ff071e235d1a5bc35a76376fb6b
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-22 18:58:09 +0100

    create

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - function
+ * @no: pionter
+ * @n: integer
+ * Return: 0
+ */
+
+void reverse_array(int *no, int n)
+{
+int i, j;
+int newarray[n];
+j = 0;
+for (i = n; i >= 0; --i)
+{
+*(newarray + j) = no[i];
+++j;
+}
+for (i = 0; i <= n; ++i)
+{
+*(no + i) = newarray[i];
+}
+}
