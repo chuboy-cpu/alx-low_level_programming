38a838c94cfe0dfaff920e68b4c21521c977874a
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-22 05:35:57 +0100

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+
+int main()
+{
+int j;
+char s[] = "hwy";
+char t[] = "hwy";
+j = _strcmp(t, s);
+printf("%d", j);
+}
 
 /**
  * _strcmp - function
@@ -9,16 +19,9 @@
 
 int _strcmp(char *s1, char *s2)
 {
-if (s1 != s2)
-{
-if (s2 > s1)
-{
-return (s2 - s1);
-}
-else
+if (*s1 != *s2)
 {
 return (s1 - s2);
-}
 }
 return (0);
 }
