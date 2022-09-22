96c6a48de71915242f234aaebe4e0f9c266db5f9
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-22 04:05:56 +0100

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - function
+ * @dest: pionter
+ * @src: pionter
+ * Return: j
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i, j;
+i = 0;
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
