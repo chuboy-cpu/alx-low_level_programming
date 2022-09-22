e765437156b70473658cc786d80ca241b6cb19f3
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-22 20:12:03 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,10 @@
 char *string_toupper(char *n)
 {
 int i;
-char *cap;
+
 for (i = 0; n[i] != '\0'; ++i)
 {
-*(cap + i) = n[i] - 32;
+*(n + i) = n[i] - 32;
 }
-printf("%s", cap);
-return (cap);
+return (n);
 }
