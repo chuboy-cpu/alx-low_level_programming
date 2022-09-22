32e489e6fad5dbb99e8d49fd3a89bfaabcf0a898
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-22 20:14:23 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * reverse_array - function
- * @no: pionter
- * @n: integer
- * Return: 0
+ * string_toupper - function
+ * @n: char
+ * Return: n
  */
 
 char *string_toupper(char *n)
 {
 int i;
-
 for (i = 0; n[i] != '\0'; ++i)
 {
+if (n[i] >= 'a' && n[i] <= 'z')
+{
 *(n + i) = n[i] - 32;
+}
 }
 return (n);
 }
