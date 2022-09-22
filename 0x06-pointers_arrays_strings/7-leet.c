6a2de408ecb68ae7146b6db24dd6697e6d58c5df
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-22 20:18:29 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,26 +9,25 @@
 char *leet(char *n)
 {
 int i;
-char *cap;
 for (i = 0; n[i] != '\0'; ++i)
 {
 if (n[i] == 'a' || n[i] == 'A')
 {
 n[i] = '4';
 }
-if (n[i] == 'e' || n[i] == 'E')
+else if (n[i] == 'e' || n[i] == 'E')
 {
 n[i] = '3';
 }
-if (n[i] == 'o' || n[i] == 'O')
+else if (n[i] == 'o' || n[i] == 'O')
 {
 n[i] = '0';
 }
-if (n[i] == 't' || n[i] == 'T')
+else if (n[i] == 't' || n[i] == 'T')
 {
 n[i] = '7';
 }
-if (n[i] == 'l' || n[i] == 'L')
+else if (n[i] == 'l' || n[i] == 'L')
 {
 n[i] = '1';
 }
