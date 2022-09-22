4bef5aee3dcc6845cac4905d70ab2145b896346d
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-22 20:02:05 +0100

    create

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * _strcmp - function
- * @s1: pionter
- * @s2: pionter
- * Return: 0
+ * _strcmp - compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 0 is strings are equal else, defference between strings
  */
-
 int _strcmp(char *s1, char *s2)
 {
-if (*s1 != *s2)
-{
-return (*s1 - *s2);
-}
-return (0);
+	for (; *s1 != '\0' && *s2 != '\0'; s2++)
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+	if (*s1 != '\0')
+		return (*s1);
+	else if (*s2 != '\0')
+		return (*s2);
+
+	return (0);
 }
