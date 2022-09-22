a08144ce4c99373dfa9a064a944f7eb88ed33fb3
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-22 05:20:02 +0100

    e

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,34 @@
 #include "main.h"
 
 /**
+ * _strncpy - function
+ * @dest: pionter
+ * @src: pionter
+ * @n: integer
+ * Return: j
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i, j;
+i = 0;
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = 0;
+++i;
+}
+return (dest);
+}
+
+
+#include "main.h"
+
+/**
  * _strncpy - copies as string
  * @n: number of bytes
  * @dest: string copied
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,6 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i = 0;
 if (s1 != s2)
 {
 if (s2 > s1)
