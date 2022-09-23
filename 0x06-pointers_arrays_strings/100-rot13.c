065d01fe3f72ced6986719e7df5bcacc3134cd93
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-23 12:56:57 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,19 +3,22 @@
 /**
  * rot13 - encodes a string into 1337
  * @str: string to be processed
- * Description:
  * Return: string
  */
 
 char *rot13(char *str)
 {
-	int x;
-	for (x = 0; str[x] != '\0'; x++)
-	{
-        if (str[x] >= 'a' && str[x] <= 'z')
-        {
-            str[x] = (str[x] + 12);
-        }
-    }
-	return (str);
+int x;
+for (x = 0; str[x] != '\0'; x++)
+{
+if (str[x] >= 'a' && str[x] <= 'm')
+{
+str[x] = (str[x] + 12);
+}
+else
+{
+str[x] = (str[x] - 12);
+}
+}
+return (str);
 }
