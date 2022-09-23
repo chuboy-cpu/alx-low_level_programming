ee9a14a2c5238c32c04d412bea9c571ecc979107
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-23 14:53:06 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,20 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-for (; *s1 != '\0' && *s2 != '\0'; s2++)
+int i = 0;
+while (*(s1 + i) == *(s2 + i) && *(s1 + i) != '\0')
 {
-if (*s1 != *s2)
-{
-return (*s1 - *s2);
+++i;
 }
-}
-if (*s1 != '\0')
-{
-return (*s1);
-}
-else if (*s2 != '\0')
-{
-return (*s2);
-}
-return (0);
+return (s1[i] - s2[i]);
 }
