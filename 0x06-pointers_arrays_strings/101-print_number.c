c6575d1da91b7da76dc859f70a39a16e18fe7688
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-23 17:16:58 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,16 @@
 
 void print_number(int n)
 {
+unsigned int x;
+x = n;
 if (n < 0)
 {
 putchar('-');
-n = -n;
+x = -n;
 }
-if (n / 10 != 0)
+if (x / 10 != 0)
 {
-print_number(n / 10);
+print_number(x / 10);
 }
-putchar((n % 10) + '0');
+putchar((x % 10) + '0');
 }
