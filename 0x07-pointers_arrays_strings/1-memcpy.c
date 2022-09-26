f03eb78af90762a635f555e175cf0a8bbaca616e
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-26 21:54:44 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * memcpy - function
+ * _memcpy - function
  * @dest: pionter
  * @src: pionter
  * @n: integer
  * Return: j
  */
 
-char *memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 int i;
 for (i = 0; i < n; ++i)
