48b5d6b5d3013a3217fb94d18e2a4f6b66269d86
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-09-27 05:17:12 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,18 +8,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-int x, y, m;
-m = 0;
+int x, y;
 for (x = 0; x < 8; x++)
 {
 for (y = 0; y < 8; y++)
 {
-if (x > m)
-{
-_putchar('\n');
-++m;
-}
 _putchar(a[x][y]);
 }
+_putchar('\n');
 }
 }
