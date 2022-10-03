2e38a5efa253f398bc778fac0cec8972da942ab9
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-03 18:30:56 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,8 +11,8 @@
  *
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((__unused__)))
 {
-	printf("%d\n", argc - 1);
+	printf("%d\n", (argc - 1));
 	return (0);
 }
