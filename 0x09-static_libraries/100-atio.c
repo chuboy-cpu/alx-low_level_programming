f4f9ba5ff5177d022c4819149e8c713ee4e0a91a
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-03 18:33:34 +0100

    Update 100-atio.c

--- 0x09-static_libraries/100-atio.c
+++ 0x09-static_libraries/100-atio.c
@@ -5,14 +5,12 @@
  * @s: char type string
  * Return: integer converted
  */
-
 int _atoi(char *s)
 {
 	int i;
 	int res = 0;
 	int sig = -1;
 	int brk = 0;
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
