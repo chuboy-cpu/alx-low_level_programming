31518f0008069fb1dd5b36420984543c273a8693
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-03 18:40:20 +0100

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print name of program, followed by a new line
+ *
+ * @argc: arguement count
+ * @argv: arguement array
+ *
+ * Description: prints the name of the program
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+    int i;
+	for (i = 0; i < argc; ++i)
+    {
+	printf("%s\n", argv[0]);
+    }
+    return (0);
+}
