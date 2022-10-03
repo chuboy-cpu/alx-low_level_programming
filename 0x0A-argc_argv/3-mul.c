f17ed745df3ebda3e24cf3cd30d5adaaaa7bf83f
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-03 19:35:50 +0100

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints all arguements it receives
+ *
+ * @argc: arguement count
+ * @argv: arguement array
+ *
+ * Description: print all arguements paased on a new line
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+int x;
+
+if (argc < 3)
+{
+printf("Error");
+return (1);
+}
+printf("%d", atio(argv[1]) * atio(argv[2]));
+return (0);
+}
