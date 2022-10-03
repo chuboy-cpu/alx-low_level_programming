90039ec71dd54c76ea5f5fb4765614b8ea6423ba
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-03 20:31:59 +0100

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints all arguements it receives
+ *
+ * @argc: arguement count
+ * @argv: arguement array
+ *
+ * Description: print all arguements paased on a new line
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0;
+if (argc <= 1)
+{
+printf("0\n");
+}
+else
+{
+int i;
+for (i = 0; i < argc; ++i)
+{
+if (argv[i] >= 48 && argv[i] <= 57)
+{
+sum += atoi(argv[i]);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("%d\n", sum);
+}
+return (0);
+}
