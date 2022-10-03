71cac1d30232f1450abd4752bc3ef3a11bffbe43
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-03 11:55:05 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,9 @@
 
 /**
  * main - print name of program, followed by a new line
- *
  * @argc: arguement count
  * @argv: arguement array
- *
  * Description: prints the name of the program
- *
  * Return: 0 on success
  */
 
