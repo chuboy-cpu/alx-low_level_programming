9b6d9c0e60ef7589634a2e0f65ae2fdde2215d36
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-08 16:50:01 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,29 +3,24 @@
 
 /**
  * array_range - creates an array of integers
- * @min: char to initialize array
- * @max: integer
- * Return: NULL if size = 0, NULL if it fails
+ *
+ * @min: minimum integer
+ * @max: maximum integer
+ *
+ * Return: pointer to newly created array
+ *
  */
 int *array_range(int min, int max)
 {
-int *p;
-if (min > max)
-{
-return (NULL);
-}
-int i;
-p = malloc(sizeof(int) * (max - min + 1));
-if (!p)
-{
-return (NULL);
-}
-i = 0;
-while (min <= max)
-{
-*(p + i) = min;
-++i;
-++min;
-}
-return (p);
+	int *ptr;
+	int x;
+
+	if (min > max)
+		return (NULL);
+	ptr = malloc(sizeof(int) * (max - min + 1));
+	if (!ptr)
+		return (NULL);
+	for (x = 0; x <= (max - min); x++)
+		ptr[x] = min + x;
+	return (ptr);
 }
