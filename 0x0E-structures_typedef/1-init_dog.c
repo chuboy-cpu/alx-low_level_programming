d1b09dddde42456c4308ad9303f3ef0962e67b4d
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-10 23:00:00 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,12 +2,10 @@
 
 /**
  * init_dog - dog definition
- * 
  * @d: struct
  * @name: name
  * @age: age
  * @owner: owner
- * 
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
