76bfeb4233fa55addcd474e6582231c4e149dc79
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-11 05:17:06 +0100

    vreate

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - function
+ * @dest: pionter
+ * @src: pionter
+ * @n: integer
+ * Return: j
+ */
+
+char *_strncpy(char *dest, char *src)
+{
+int i;
+i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
+
+/**
+ * print_dog - print a struct dog
+ *
+ * @d: pointer to struct dog
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *my_dog;
+my_dog = malloc(sizeof(struct dog));
+if (my_dog == NULL)
+{
+return (NULL);
+}
+_strncpy(my_dog->name, name);
+_strncpy(my_dog->owner, owner);
+my_dog->age = age;
+}
