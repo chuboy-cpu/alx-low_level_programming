fcd805267b667c3f45bbe7e345971ce36327acbf
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-23 00:05:32 +0100

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,20 +10,17 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t i;
-i = 0;
-while (h != NULL)
-{
-if (h->str == NULL)
-{
-printf("[%d] (%s)\n", 0, "nil");
-}
-else
-{
-printf("[%d] %s\n", h->len, h->str);
-}
-h = h->next;
-++i;
-}
-return (i);
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
