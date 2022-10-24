f57265be214088e08a239b870c81a460e256207d
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-24 18:53:05 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
- * print_list - print all the elements of a list
+ * print_listint - print all the elements of a list
  *
  * @h: head
  *
  * Return: number of nodes
- *
  */
 size_t print_listint(const listint_t *h)
 {
