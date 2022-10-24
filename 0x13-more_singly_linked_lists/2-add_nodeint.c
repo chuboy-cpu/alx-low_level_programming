0775adbb298a440a0cf3df82f6e2fe71c23803ae
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-24 21:37:39 +0100

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,15 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a list
+ * listint_t - add nodes to the begining of a list
  *
  * @h: head
- *
+ * @n: element
  * Return: number of nodes
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *new_node = malloc(sizeof(listint_t));
+if (!new_node)
+{
+return (NULL);
+}
 new_node->n = n;
 new_node->next = *head;
 *head = new_node;
