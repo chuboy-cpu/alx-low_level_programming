d7ea67cd106d9641855fe43e7a8146c2219d5ca0
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-24 21:50:49 +0100

    Create 3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add nodes to the begining of a list
+ *
+ * @head: head
+ * @n: element
+ * Return: number of nodes
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *hold = *head;
+listint_t *new_node = malloc(sizeof(listint_t));
+if (!new_node)
+{
+return (NULL);
+}
+new_node->n = n;
+while (hold != NULL)
+{
+hold = hold->next;
+}
+hold->next = new_node;
+
+return (new_node);
+}
