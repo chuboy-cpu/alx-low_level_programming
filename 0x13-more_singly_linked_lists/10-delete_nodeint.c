ef241973212fcc65bd8bd12fe99558ecbb812a44
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-25 22:30:34 +0100

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,33 +1,40 @@
 #include "lists.h"
 
-
 /**
- * pop_listint - deletes the head node of a linked list
- *
- * @head: pointer
- *
- * Return: head nodes data(n)
- *
+ * delete_nodeint_at_index - A function that deletes the node at
+ * specified index from list
+ * @head: A pointer to listint_t structure
+ * @index: The index of the node to delete
+ * Return: 1 if succeeded, or -1 if it failed
+ * temp-hold, dnode-new_node, i-Bello.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *hold;
-int n;
-size_t i;
-i = 0;
+	listint_t *hold, *new_node;
+	unsigned int Bello = 1;
 
-if (*head == NULL)
-{
-return (-1);
-}
-while (i < index)
-{
-*head = (*head)->next;
-i++;
-}
-hold = *head;
-hold = (*head)->next;
-free(*head);
-*head = hold;
-return (1);
+	if (*head == NULL)
+		return (-1);
+
+	hold = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(hold);
+	}
+	else
+	{
+		while (Bello < index)
+		{
+			hold = hold->next;
+			if (hold == NULL)
+				return (-1);
+			Bello++;
+		}
+		new_node = hold;
+		new_node = new_node->next;
+		hold->next = new_node->next;
+		free(new_node);
+	}
+	return (1);
 }
