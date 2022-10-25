663a37af7f7a910eb5884a74635cb56884f76acd
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-25 22:02:35 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,33 +8,32 @@
  * @idx: index to put the new node
  * Return: number of nodes
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
-size_t i;
-i = 0;
-listint_t *new_node = malloc(sizeof(listint_t));
-if (!new_node)
-{
-return (NULL);
-}
-new_node->n = n;
-if (idx == 0)
-{
-new_node->next = *head;
-*head = new_node;
-return (*head);
-}
-while (i < idx)
-{
-*head = (*head)->next;
-++i;
-if (!(*head))
-{
-free(new_node);
-return (NULL);
-}
-}
-new_node->next = (*head)->next;
-*head = new_node;
-return (new_node);
+	listint_t *hold, *new_node;
+
+	hold = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (index == 0)
+	{
+		new_node->next = hold;
+		*head = new_node;
+		return (*head);
+	}
+	while (index > 1)
+	{
+		hold = hold->next;
+		index--;
+		if (!hold)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	new_node->next = hold->next;
+	hold->next = new_node;
+	return (new_node);
 }
