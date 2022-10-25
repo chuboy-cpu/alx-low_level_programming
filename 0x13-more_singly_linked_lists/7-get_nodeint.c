d71df4c3e12987e0da2d26d37107f2713a2de4fe
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-25 20:25:09 +0100

    Create 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - add nodes to the begining of a list
+ *
+ * @head: head
+ * @n: element
+ * Return: number of nodes
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *hold;
+size_t i;
+i = 0;
+if (head == NULL)
+{
+return (NULL);
+}
+else
+{
+while (index >= i)
+{
+hold = head->next;
+head = hold;
+++i;
+}
+}
+return (head->n);
+}
