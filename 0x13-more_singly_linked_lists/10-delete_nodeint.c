034f1bdb98b6e7e84e87bb70803e8178cc5a2c1a
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-25 22:24:32 +0100

    Create 10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+
+/**
+ * pop_listint - deletes the head node of a linked list
+ *
+ * @head: pointer
+ *
+ * Return: head nodes data(n)
+ *
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *hold;
+int n;
+size_t i;
+i = 0;
+
+if (*head == NULL)
+{
+return (0);
+}
+while (i < index)
+{
+*head = (*head)->next;
+i++;
+}
+hold = *head;
+hold = (*head)->next;
+free(*head);
+*head = hold;
+return (n);
+}
