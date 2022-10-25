a64f3c1854d5e10746a674580073489c522f39af
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-25 21:09:26 +0100

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,13 +7,13 @@
  *
  * Return: number of nodes
  */
-int sum_listint(const listint_t *head)
+int sum_listint(listint_t *head)
 {
 int sum;
 sum = 0;
 if (head == NULL)
 {
-return (NULL);
+return (0);
 }
 while (head != NULL)
 {
