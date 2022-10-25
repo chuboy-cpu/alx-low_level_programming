b4c3e7d34bb0a9c1d03a97464a3cf6360d72473a
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-25 20:54:38 +0100

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,16 +9,15 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-size_t i;
-i = 0;
-if (head == NULL)
-{
-return (NULL);
-}
-while (index > i)
-{
-head = head->next;
-++i;
-}
-return (head);
+	size_t n;
+
+	n = 0;
+	while (n < index)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+		n++;
+	}
+	return (head);
 }
