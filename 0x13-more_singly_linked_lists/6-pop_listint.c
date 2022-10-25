bfed27dff77e970bd7eef92ba96a98f7915a60bb
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2022-10-25 20:37:33 +0100

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,10 +1,13 @@
 #include "lists.h"
 
+
 /**
- * pop_listint -frees a list
+ * pop_listint - deletes the head node of a linked list
  *
- * @head: head
+ * @head: pointer
+ *
  * Return: head nodes data(n)
+ *
  */
 int pop_listint(listint_t **head)
 {
